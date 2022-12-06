39a7611160c35207905609fda76d8c3edf3fbfd9
SyedAbdulrab <syedabdulrab1133@gmail.com>
2022-12-06 17:56:30 +0500

    Ready for deployment 2

---
+++ .next/static/chunks/pages/Abdulrab/Abdulrab-a502e4a8e1b11fb8ba15.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[4],{LmNM:function(n,_,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Abdulrab/Abdulrab",function(){return w("i356")}])},i356:function(n,_){}},[["LmNM",0]]]);
---
+++ .next/static/chunks/pages/Abdulrab/Customers-218e8c15951810970dc9.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[5],{Rxgx:function(n,_){},fDWA:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Abdulrab/Customers",function(){return o("Rxgx")}])}},[["fDWA",0]]]);
